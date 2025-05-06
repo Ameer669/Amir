8bf091a939c61d90a47d0a11acf4ed32055271e5
Amir <37736638+Ameer669@users.noreply.github.com>
2025-05-06 18:35:45 +0500

    Add files via upload

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,25 +1,71 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-int search(int arr[], int size, int target){
-	for(int i = 0; i < size; i++){
-		if(arr[i] == target){
-			return i;
-		}
-	}
-	return -1;
-}
+class employee {
+    protected:
+        string name;
+        int number;
+    public:
+        employee(string n =" ", int no = 0){
+            name = n;
+            number = no;
+        }
+        void getdata(){
+            cout<<"Enter The name: "<<name;
+            cin>>name;
+            cout<<"Enter The number: "<<number;
+            cin>>number;
+        }
+        void showdata(){
+            cout<<"\n--Name: "<<name<<"\nNumber: "<<number<<endl;
+        }
+};
 
-int main() {
-   int arr[] = {1,4,68,1,6,8,32,76,93};
-   int size = sizeof(arr) / sizeof(arr[0]);
-   
-   int index = search(arr, size, 32);
-   if(index != -1){
-		cout<<"Number Found At Index: "<<index;
-   }else{
-   	cout<<"Number Not Found!";
-   }
+class manager : public employee{
+    private:
+        string title;
+        double dues;
+    public:
+        manager(string n = " ", int no = 0, string ti= " ", double du = 0.0) : employee(n,no){
+            title = ti;
+            dues = du;
+        }
+        void getdata(){
+            employee::getdata();
+            cout<<"Enter The title: "<<title;
+            cin>>title;
+            cout<<"Enter The dues: "<<dues;
+            cin>>dues;
+        }
+        void showdata(){
+            employee::showdata();
+            cout<<"title: "<<title<<"\ndues: "<<dues<<endl;
+        }
+};
+class scientist : public employee{
+    private:
+        int pubs;
+    public:
+        scientist(string n = " ", int no = 0, int pu = 0) : employee(n,no){
+            pubs = pu;
+        }
+        void getdata(){
+            employee::getdata();
+            cout<<"Enter The pubs: "<<pubs;
+            cin>>pubs;
+        }
+        void showdata(){
+            employee::showdata();
+            cout<<"publications: "<<pubs<<endl;
+        }
+};
+
+int main(){
+    manager man;
+    man.showdata();
+    manager man2("Amir", 123, "King", 500.0);
+    man2.showdata();
     return 0;
 }
- 
